f1859a0f12588d8875a9572c3aa65bd6c61f1a02
Youssef Moawad <youssef_programming@icloud.com>
2017-11-08 16:47:13 +0000

    Login screen done. Doesn't send REST request to actually login but looks alright.

--- ymca_app/screens/LoginScreen/LoginScreen.js
+++ ymca_app/screens/LoginScreen/LoginScreen.js
@@ -1,22 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { styles } from './styles'
+import { StyleSheet, Text, View, Button, Image, TextInput, TouchableWithoutFeedback, TouchableOpacity, Keyboard } from 'react-native';
+import { styles } from './styles';
+import PropTypes from 'prop-types';
 
 export default class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: '', password: ''};
+  }
+
+  loginButtonPressed() {
+    navigate('Meetings')
+  }
+
+  setEmailAddress(email) {
+    this.setState({email: email, password: this.state.password})
+  }
+  setPassword(password) {
+    this.setState({email: this.state.email, password: password})
+  }
+
   static navigationOptions = {
-    title: 'YMCA',
+    title: 'YMCA Mentor Login',
   };
   render() {
     const { navigate } = this.props.navigation;
     return(
-      <View style={styles.container}>
-        <Text>Welcome to YMCA Login!</Text>
-        <Button
-          onPress={() => {navigate('Meetings')}}
-          title="Go"
-          color="#841584"
-        />
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Image
+            source={require('../../images/ymca_logo.png')}
+            style={styles.logo}
+          />
+          <TextInput style={styles.loginField} placeholder="  Email" keyboardType='email-address' onChangeText={(text) => this.setState()} />
+          <TextInput style={styles.loginField} placeholder="  Password" secureTextEntry={true} />
+
+          <TouchableOpacity
+            onPress={() => {navigate("Meetings")}}
+            style={{marginTop: '7%'}}
+          >
+            <View style={{
+              flex: 0.25,
+              width: 350,
+              backgroundColor:'#0075ff',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 10,
+            }}>
+              <Text style={{
+              color:'white',
+              fontSize: 20,
+            }}>
+                Sign In
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
+}
+
+LoginScreen.PropTypes = {
+  email: PropTypes.string,
+  password: PropTypes.string
 }
--- ymca_app/screens/LoginScreen/styles.js
+++ ymca_app/screens/LoginScreen/styles.js
@@ -1,10 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  logo: {
+    transform: [{scale: 0.6}],
+    marginTop: '10%'
+  },
+
+  loginField: {
+    width: '60%',
+    height: 50,
+    borderBottomWidth: 2,
+    borderBottomColor: 'gray',
+    marginTop: '2.5%',
+    marginBottom: '2.5%',
+  },
+
   container: {
+    flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
