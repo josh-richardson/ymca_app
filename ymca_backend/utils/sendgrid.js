322016a18cd34764864d569b456c594c9370d1c4
Joshua Richardson <joshua@richardson.tech>
2017-11-15 09:37:52 +0000

    changes email from mine to default

--- ymca_backend/utils/sendgrid.js
+++ ymca_backend/utils/sendgrid.js
@@ -4,7 +4,7 @@ const sendEmail = function (to, subject, message) {
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(config.sendgrid_api_key);
     const msg = {
-        to: "richardsonjoshua228@gmail.com",
+        to: to,
         from: 'test@example.com',
         subject: subject,
         text: message,
