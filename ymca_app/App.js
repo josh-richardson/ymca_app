8a649a0a2f93db980630e4ef57c25a63adbcbfee
Youssef Moawad <youssef_programming@icloud.com>
2017-12-05 16:54:48 +0000

    Reconfigured routes.js to be able to display modal screens. Now displays Emergency as modal.

--- ymca_app/App.js
+++ ymca_app/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator, } from 'react-navigation';
-import { Stack } from './config/routes';
+import Root from './config/routes';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Stack />
+      <Root />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- ymca_app/config/routes.js
+++ ymca_app/config/routes.js
@@ -3,25 +3,19 @@ import { StackNavigator } from 'react-navigation';
 
 import { LoginScreen, MainScreen, MeetingsScreen, MeetingDetailsScreen, ScheduleAppointmentScreen, EmergencyAlertSentScreen } from '../screens/';
 
-export const Stack = StackNavigator({
-  Login: {
-    screen: LoginScreen
-  },
-  Main: {
-    screen: MainScreen
-  },
-  Meetings: {
-    screen: MeetingsScreen
-  },
-  MeetingDetails: {
-    screen: MeetingDetailsScreen
-  },
-  ScheduleAppointment: {
-    screen: ScheduleAppointmentScreen
-  },
-  EmergencyAlertSent: {
-    screen: EmergencyAlertSentScreen
-  }
+const MainStackNavigator = StackNavigator({
+  Login: { screen: LoginScreen },
+  Main: { screen: MainScreen },
+  Meetings: { screen: MeetingsScreen },
+  MeetingDetails: { screen: MeetingDetailsScreen },
+  ScheduleAppointment: { screen: ScheduleAppointmentScreen },
+})
+
+export default Root = StackNavigator({
+  Stack: { screen: MainStackNavigator },
+
+  EmergencyAlertSent: { screen: EmergencyAlertSentScreen }
 }, {
-  mode: 'card',
-});
+  mode: 'modal',
+  headerMode: 'none',
+})
