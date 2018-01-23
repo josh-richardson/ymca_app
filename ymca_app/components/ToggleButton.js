8bcbb36a2c7720784d3863cc7377978bdfbac11c
Youssef Moawad <youssef_programming@icloud.com>
2018-01-23 17:35:09 +0000

    Fixes happy/sad faces' ToggleButtons.

--- ymca_app/components/ToggleButton.js
+++ ymca_app/components/ToggleButton.js
@@ -4,24 +4,11 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 export default class ToggleButton extends React.Component {
   constructor() {
     super()
-
-    this.state = {
-      toggled: false
-    }
   }
 
   pressed() {
-    if(!this.props.hasOwnProperty("toggled")) { this.setState({ toggled: !this.state.toggled }) }
-
     if(this.props.onPress) this.props.onPress()
-  }
-
-  componentWillReceiveProps() {
-    console.log(this.props.toggled)
-
-    if(this.props.hasOwnProperty("toggled")) {
-      this.setState({ toggled: this.props.toggled })
-    }
+    else console.log("WARNING: onPress for ToggleButton is not set.")
   }
 
   render() {
@@ -33,7 +20,7 @@ export default class ToggleButton extends React.Component {
       <View style={{
         height: this.props.size || 45,
         width: this.props.size || 45,
-        backgroundColor:this.state.toggled ? 'grey' : 'white',
+        backgroundColor: this.props.toggled ? 'grey' : 'white',
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: (this.props.size || 45)/2,
