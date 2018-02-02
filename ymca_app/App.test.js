7f40cac4e3a413abdee303986f21a9c87b83745d
Youssef Moawad <youssef_programming@icloud.com>
2018-02-02 23:10:38 +0000

    Testing if Jenkins will fail #56

--- ymca_app/App.test.js
+++ ymca_app/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import App from './App';
+import React from 'react'
+import App from './App'
 
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
+  expect(rendered).toBeTruthy()
+})
+
+it('this test should fail', () => {
+  expect(false).toBeTruthy()
+})
