168a039abc463eee31cde47c0cbed06d4c91f221
Youssef Moawad <youssef_programming@icloud.com>
2018-02-03 00:45:49 +0000

    Added unit tests for mentee reducer #55

--- ymca_app/tests/App.test.js
+++ ymca_app/tests/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import App from './App'
+import App from '../App'
 
 import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy()
-})
-
-it('this test should fail', () => {
-  expect(false).toBeTruthy()
 })
---
+++ ymca_app/tests/ReduxTests/mentees.test.js
@@ -0,0 +1,67 @@
+import { store, addMentee, removeMentee, setMentees } from '../../model'
+
+beforeAll(() => {
+
+})
+
+describe('mentee test suite', () => {
+
+  test('can add mentee', () => {
+    const mentee = {id: 100, firstName: "Test", secondName: "Tester"}
+    store.dispatch(addMentee(mentee))
+
+    let state = store.getState()
+
+    expect(state.mentees[0]).not.toBeNull()
+    expect(state.mentees[0].id).toBe(100)
+    expect(state.mentees[0].firstName).toBe("Test")
+    expect(state.mentees[0].secondName).toBe("Tester")
+  })
+
+  test('can set mentees', () => {
+    const mentees = [
+      {id: 100, firstName: "Test", secondName: "Tester"},
+      {id: 200, firstName: "Test2", secondName: "Tester2"},
+      {id: 300, firstName: "Test3", secondName: "Tester3"},
+    ]
+    store.dispatch(setMentees(mentees))
+
+    let state = store.getState()
+
+    expect(state.mentees[0]).not.toBeNull()
+    expect(state.mentees[0].id).toBe(100)
+    expect(state.mentees[0].firstName).toBe("Test")
+    expect(state.mentees[0].secondName).toBe("Tester")
+
+    expect(state.mentees[1]).not.toBeNull()
+    expect(state.mentees[1].id).toBe(200)
+    expect(state.mentees[1].firstName).toBe("Test2")
+    expect(state.mentees[1].secondName).toBe("Tester2")
+
+    expect(state.mentees[2]).not.toBeNull()
+    expect(state.mentees[2].id).toBe(300)
+    expect(state.mentees[2].firstName).toBe("Test3")
+    expect(state.mentees[2].secondName).toBe("Tester3")
+  })
+
+  test('can remove mentees', () => {
+    const mentees = [
+      {id: 100, firstName: "Test", secondName: "Tester"},
+      {id: 200, firstName: "Test2", secondName: "Tester2"},
+      {id: 300, firstName: "Test3", secondName: "Tester3"},
+    ]
+
+    store.dispatch(setMentees(mentees))
+
+    store.dispatch(removeMentee(100))
+    expect(store.getState().mentees[0].id).toBe(200)
+    expect(store.getState().mentees[1].id).toBe(300)
+
+    store.dispatch(removeMentee(200))
+    expect(store.getState().mentees[0].id).toBe(300)
+
+    store.dispatch(removeMentee(300))
+
+    expect(store.getState().mentees).toHaveLength(0)
+  })
+})
