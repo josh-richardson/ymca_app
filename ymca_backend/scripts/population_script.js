e1902f6d3d49dec596eb378d92095f2b30dcc3e3
Joshua Richardson <joshua@richardson.tech>
2018-03-01 20:51:05 +0000

    Adds completely rewritten population script - again

--- ymca_backend/scripts/population_script.js
+++ ymca_backend/scripts/population_script.js
@@ -5,6 +5,7 @@ const manager = require('../models/users/manager');
 const meeting = require('../models/meeting');
 const admin = require('../models/users/admin');
 const mentor = require('../models/users/mentor');
+var faker = require('faker');
 
 const config = require('../config/config');
 process.env.NODE_ENV = 'test';
@@ -27,29 +28,169 @@ mongoose.connect(config.db_path, {
     });
 
 
-const adminDetails = {
-    'email': "test@gmail.com",
-    'password': "password123",
-    'firstName': 'Jack',
-    'secondName': 'Jones',
-    'phone': '07450760208'
+const basePassword = "password";
+let passCounter = 0;
+
+const createAdmin = (details) => {
+    return new Promise((resolve, reject) => {
+        const adminUser = new user();
+        adminUser.email = details.email;
+        adminUser.password = adminUser.hashPassword(details.password);
+        const newAdmin = new admin();
+        newAdmin.firstName = details.firstName;
+        newAdmin.secondName = details.secondName;
+        newAdmin.phone = details.phone;
+        newAdmin.save((err, userResult) => {
+            if (err) {
+                console.log(err)
+            } else {
+                adminUser.linkedModel = newAdmin;
+                adminUser.save(function (err, result) {
+                    resolve(userResult);
+                });
+            }
+        });
+
+    });
 };
 
-const menteeDetails = {
-    'email': "mentee@gmail.com",
-    'firstName': "Javeth",
-    'secondName': "Jared",
-    'phone': '07450760408',
-    'meetingAddress': 'Some scottish road somewhere'
+
+const createManager = (details) => {
+    return new Promise((resolve, reject) => {
+        const managerUser = new user();
+        managerUser.email = details.email;
+        managerUser.password = managerUser.hashPassword(details.password);
+        const newManager = new manager();
+        newManager.firstName = details.firstName;
+        newManager.secondName = details.secondName;
+        newManager.phone = details.phone;
+        newManager.save((err, userResult) => {
+            if (err) {
+                console.log(err)
+            } else {
+                managerUser.linkedModel = newManager;
+                managerUser.save(function (err, result) {
+                    resolve(userResult);
+                });
+            }
+        });
+    });
 };
 
-const mentee2Details = {
-    'email': "mentee2@gmail.com",
-    'firstName': "Jeff",
-    'secondName': "Bezos",
-    'phone': '07450760408',
-    'meetingAddress': '12 Scotland St.'
+
+const createMentor = (details, manager) => {
+    return new Promise((resolve, reject) => {
+        const newMentor = new mentor();
+        newMentor.firstName = details.firstName;
+        newMentor.secondName = details.secondName;
+        newMentor.phone = details.phone;
+        newMentor.manager = manager;
+        const mentorUser = new user();
+        mentorUser.email = details.email;
+        mentorUser.password = mentorUser.hashPassword(details.password);
+        newMentor.save((err, userResult) => {
+            if (err) {
+                console.log("Error:" + err)
+            } else {
+                mentorUser.linkedModel = newMentor;
+                mentorUser.save((err, result) => {
+                    resolve(userResult)
+                });
+            }
+        });
+    });
 };
+
+
+const createMentee = (details, mentor) => {
+    return new Promise((resolve, reject) => {
+        const newMentee = new mentee();
+        newMentee.firstName = details.firstName;
+        newMentee.secondName = details.secondName;
+        newMentee.phone = details.phone;
+        newMentee.meetingAddress = details.meetingAddress;
+        newMentee.mentor = mentor;
+        newMentee.save((err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+};
+
+
+const createMeeting = (mentorUser, menteeUser) => {
+    return new Promise((resolve, reject) => {
+        const newMeeting = new meeting();
+        newMeeting.mentor = mentorUser;
+        newMeeting.mentee = menteeUser;
+        newMeeting.meetingAddress = menteeUser.meetingAddress;
+        newMeeting.startTime = new Date();
+        newMeeting.endTime = new Date();
+        newMeeting.save(function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                resolve(result);
+            }
+        });
+    });
+};
+
+
+for (let i = 0; i < 3; i++) {
+    let adminEmail = faker.internet.email();
+    createAdmin({
+        firstName: faker.name.firstName(),
+        secondName: faker.name.lastName(),
+        email: adminEmail,
+        phone: faker.phone.phoneNumber().replace("-", ""),
+        password: adminEmail
+    }).then((admin) => {
+        console.log(admin);
+    });
+
+    let managerEmail = faker.internet.email();
+    createManager({
+        firstName: faker.name.firstName(),
+        secondName: faker.name.lastName(),
+        email: managerEmail,
+        phone: faker.phone.phoneNumber().replace("-", ""),
+        password: managerEmail
+    }).then((manager) => {
+
+        for (let j = 0; j < 3; j++) {
+            let mentorEmail = faker.internet.email();
+            createMentor({
+                firstName: faker.name.firstName(),
+                secondName: faker.name.lastName(),
+                email: mentorEmail,
+                phone: faker.phone.phoneNumber().replace("-", ""),
+                password: mentorEmail
+            }, manager).then((mentor) => {
+                console.log(mentor);
+                for (let k = 0; k < 3; k++) {
+                    createMentee({
+                        firstName: faker.name.firstName(),
+                        secondName: faker.name.lastName(),
+                        meetingAddress: faker.address.streetAddress(),
+                        phone: faker.phone.phoneNumber().replace("-", ""),
+                    }, mentor).then((mentee) => {
+                        console.log(mentee);
+                        createMeeting(mentor, mentee).then((meeting) => {
+                            console.log(meeting);
+                        });
+                    });
+                }
+            })
+        }
+    });
+}
+
+
+/*
 
 const newUser = new user();
 newUser.email = adminDetails.email;
@@ -99,21 +240,6 @@ newAdmin.save((err, result) => {
             }
         });
     }
-});
+});*/
 
 
-//
-// newMentee.save(function (err, result) {
-//     if (!err) {
-//
-//     }
-// });
-// const newMentee2 = new mentee();
-// newMentee2.email = mentee2Details.email;
-// newMentee2.firstName = mentee2Details.firstName;
-// newMentee2.secondName = mentee2Details.secondName;
-// newMentee2.phone = mentee2Details.phone;
-// newMentee2.meetingAddress = mentee2Details.meetingAddress;
-// newMentee2.mentor = newUser;
-// newMentee2.save(function (err, result) {
-// });
