b2e10acce4961e515829f00c1ff042f9b9d6ffed
Youssef Moawad <youssef_programming@icloud.com>
2018-03-03 22:27:30 +0000

    Initial implementation of Notifications class. To be tested. #95

--- ymca_app/model/index.js
+++ ymca_app/model/index.js
@@ -4,3 +4,5 @@ export StoreHydrator from './StoreHydrator'
 export Appointment from './Appointment'
 export Mentee from './Mentee'
 export Mentor from './Mentor'
+
+export Notifications from './Notifications'
