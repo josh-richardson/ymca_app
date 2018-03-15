93b436775da4fac35f2fc2759a7f1bd15450a546
Youssef Moawad <youssef_programming@icloud.com>
2018-03-15 20:22:00 +0000

    Added documentation to the Mentor model class. #104

--- ymca_app/model/Mentor.js
+++ ymca_app/model/Mentor.js
@@ -1,11 +1,25 @@
 import store from './store'
 import { setInfo } from './Actions'
 
+/**
+ * Static singleton class representing the currently logged in mentor.
+ */
 export default class Mentor {
+  /**
+   * Sets the info of the logged in mentor in the Redux store.
+   *
+   * @param {object} info - A mentor info JS object that must have the form passed back from the online API.
+   * @param {string} jwt - The JSON Web Token used for requests authentication. This is sent back from the API during the login process.
+   */
   static setInfo(info, jwt) {
     store.dispatch(setInfo({...info, jwt}))
   }
 
+  /**
+   * Retrieves the currently stored mentor info object from the Redux store.
+   *
+   * @return {object} The mentor info object.
+   */
   static getInfoObject() {
     if(store.getState().mentorInfo != {}) return store.getState().mentorInfo
     else {
@@ -14,24 +28,31 @@ export default class Mentor {
     }
   }
 
+  /** @return {string} The ID of the currently logged in mentor. */
   static get id() {
     return Mentor.getInfoObject()._id
   }
+  /** @return {string} The email of the currently logged in mentor. */
   static get email() {
     return Mentor.getInfoObject().email
   }
+  /** @return {string} The first name of the currently logged in mentor. */
   static get firstName() {
     return Mentor.getInfoObject().firstName
   }
+  /** @return {string} The second name of the currently logged in mentor. */
   static get secondName() {
     return Mentor.getInfoObject().secondName
   }
+  /** @return {string} The full name of the currently logged in mentor. */
   static get name() {
     return `${Mentor.firstName} ${Mentor.secondName}`
   }
+  /** @return {string} The phone number of the currently logged in mentor. */
   static get phone() {
     return Mentor.getInfoObject().phone
   }
+  /** @return {string} The JSON Web Token used for requests authentication of the currently logged in mentor. */
   static get jwt() {
     return Mentor.getInfoObject().jwt
   }
