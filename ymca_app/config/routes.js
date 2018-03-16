be79d7fa14173032066ee5d90b3dfa6b57c0f1f2
Youssef Moawad <youssef_programming@icloud.com>
2018-03-16 05:17:07 +0000

    Added documentation for the routes file. #104

--- ymca_app/config/routes.js
+++ ymca_app/config/routes.js
@@ -1,8 +1,13 @@
+/**
+ * @module config/routes
+ */
+
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 
 import { LoginScreen, OptionsScreen, MeetingsScreen, MeetingDetailsScreen, ScheduleAppointmentScreen, EmergencyAlertSentScreen, MentorFeedbackScreen, MenteeFeedbackScreen } from '../screens/';
 
+/** Defines the main stack navigator that gets displayed throught the root navigator. */
 const MainStackNavigator = StackNavigator({
   Login: { screen: LoginScreen },
   Options: { screen: OptionsScreen },
@@ -13,6 +18,7 @@ const MainStackNavigator = StackNavigator({
   MenteeFeedback: { screen: MenteeFeedbackScreen }
 })
 
+/** Defines the root stack navigator that is responsible for displaying other stack navigators as well as allows for modal screens to be displayed. */
 export default Root = StackNavigator({
   Stack: { screen: MainStackNavigator },
 
