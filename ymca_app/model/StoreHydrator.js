47bcd096cacf15180bf4a8dedd622aa279575502
Youssef Moawad <youssef_programming@icloud.com>
2018-03-16 02:24:03 +0000

    Set up JSDoc and updated some syntax issues from previously documented files. Also ran JSDoc and generated files in the 'out' directory. #104

--- ymca_app/model/StoreHydrator.js
+++ ymca_app/model/StoreHydrator.js
@@ -1,3 +1,9 @@
+/**
+ *  Module for the StoreHydrator class.
+ *
+ * @module model/StoreHydrator
+ */
+
 import Requests from './Requests'
 import { Appointment, Mentee, Mentor } from './'
 
@@ -5,7 +11,7 @@ import { Appointment, Mentee, Mentor } from './'
  * Class that contains a single static method to be called at login in order to hydrate the appropriate Redux store with data retrieved from the online API that is relevant to the currently logged in mentor, including mentor information, their assigned mentees and their appointments.
  */
 export default class StoreHydrator {
-  
+
   /**
    * Retrieves appropriate information from the online API that is relevant to the currently logged in mentor, including mentor information, their assigned mentees and their appointments, given the JSON Web Token that was sent by the server after login.
    *
