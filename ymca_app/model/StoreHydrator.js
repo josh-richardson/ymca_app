b403bb525dc446bd1e680c8f1cb3b83e427e7581
Youssef Moawad <youssef_programming@icloud.com>
2018-03-16 01:14:19 +0000

    Added documentation for the store hydrator. #104

--- ymca_app/model/StoreHydrator.js
+++ ymca_app/model/StoreHydrator.js
@@ -1,7 +1,16 @@
 import Requests from './Requests'
 import { Appointment, Mentee, Mentor } from './'
 
+/**
+ * Class that contains a single static method to be called at login in order to hydrate the appropriate Redux store with data retrieved from the online API that is relevant to the currently logged in mentor, including mentor information, their assigned mentees and their appointments.
+ */
 export default class StoreHydrator {
+  
+  /**
+   * Retrieves appropriate information from the online API that is relevant to the currently logged in mentor, including mentor information, their assigned mentees and their appointments, given the JSON Web Token that was sent by the server after login.
+   *
+   * @param {string} jwt - The JSON Web Token sent by the server after login.
+   */
   static async retrieveAndStoreMentorData(jwt) {
     let mentorInfo = await Requests.getMentorInformation(jwt)
     let mentees = await Requests.getMentees(jwt)
