50a91ff15d0d57325a533405cd064b3b2817e2c1
Youssef Moawad <youssef_programming@icloud.com>
2018-03-16 14:21:09 +0000

    Added base jsdoc configuration to the backend and added documentation for the config folder. #105

--- ymca_backend/config/passport.js
+++ ymca_backend/config/passport.js
@@ -1,17 +1,21 @@
+/**
+ * @module config/passport
+ */
+
 const passport = require('passport');
 const user = require('../models/user');
 const JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 const config = require('../config/config');
 
-//JWT configuration settings
+/** JWT configuration settings. */
 const jwt_conf = {
     secretOrKey: config.jwt_secret,
     jwtFromRequest: ExtractJwt.fromBodyField('auth')
 };
 
 
-//Tell passport to use JSON web tokens, tell passport to try to find users based on their token to log them in
+/** Tell passport to use JSON web tokens, tell passport to try to find users based on their token to log them in. */
 passport.use(new JwtStrategy(jwt_conf, function (jwt_payload, done) {
     user.findOne({_id: jwt_payload._id}, function (err, user) {
         if (err) {
